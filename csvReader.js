3165d208012a0a7cc0af1e117ff34216ec8269b1
2057559s <nicholassaunderson@glaroam2-171-139.wireless.gla.ac.uk>
2015-11-23 13:47:33 +0000

    Added Pie Chart page and python script

--- csvReader.js
+++ csvReader.js
@@ -42,4 +42,5 @@ function processData(allText) {
 
     }
 
+
 }
---
+++ reader.py
@@ -0,0 +1,20 @@
+__author__ = 'Nicholas Saunderson'
+import csv
+
+
+with open('Glasgow_rest_db.csv', 'rb') as f:
+    reader = csv.reader(f)
+    main_list = list(reader)
+
+    postCode_list = [];
+
+    for i in main_list:
+        splits = i[7].split()
+        postCode_list += splits
+
+
+   
+
+
+
+
